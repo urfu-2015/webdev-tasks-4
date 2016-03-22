b459d1e24be9b9118d22560aa62a8202a03a1124
danmir <danmir@me.com>
2016-03-22 16:23:38 +0500

    bem на webpack

---
+++ index.js
@@ -0,0 +1,6 @@
+require('./blocks/index.styl');
+require('./blocks/veg-card/veg-card.styl');
+require('./blocks/veg-card/__img/veg-card__img.styl');
+require('./blocks/veg-card/__input/_type/veg-card__input_type_radio.styl');
+require('./blocks/veg-card/__label/veg-card__label.styl');
+require('./blocks/veg-slider/veg-slider.styl')
