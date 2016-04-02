d3ecababe71ae560b12d512bd602fcf8a2386920
danmir <danmir@me.com>
2016-04-03 01:19:24 +0500

    Refactor blocks

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('./blocks/index.styl');
 require('./blocks/veg-card/veg-card.styl');
 require('./blocks/veg-card/__img/veg-card__img.styl');
-require('./blocks/veg-card/__input/_type/veg-card__input_type_radio.styl');
-require('./blocks/veg-card/__label/veg-card__label.styl');
+require('./blocks/veg-card/__checker/veg-card__checker.styl');
+require('./blocks/veg-card/__title/veg-card__title.styl');
 require('./blocks/veg-slider/veg-slider.styl')
