28fe6b7becabcd73ea7d0bb887752453bf03ba20
Fedor <mydearfrodo@yandex.ru>
2016-04-02 14:10:58 +0500

    post-processing and fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,25 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var autoprefixer = require('gulp-autoprefixer');
+var postcss = require('gulp-postcss');
+var colorShort = require('postcss-color-short');
+var discardComments = require('postcss-discard-comments');
+var csscomb = require('gulp-csscomb');
+var forCss = require('postcss-for');
+var hidden = require('postcss-hidden');
 
 gulp.task('one', function () {
     return gulp.src('./index.styl')
         .pipe(stylus())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(postcss([
+            hidden, colorShort, discardComments, forCss
+        ]))
+        .pipe(csscomb())
+
         .pipe(gulp.dest('./'));
 });
 
